4bbc4dbbbde71dbf1306064bac09469b2ab79ebd
ylj <aboutlistener@163.com>
2021-01-19 18:21:06 +0800

    excel读

--- README.md
+++ README.md
@@ -7,15 +7,20 @@ excel基本操作
 
 > apache提供的，由JAVA编写的免费的跨平台API
 >
-> 提供API给JAVA程序对Microsoft Office格式档案读写功能
+> 提供API给JAVA程序对Microsoft Office格式档案读写功能(word，ppt，xls，xlsx)
+>
+> 而jxl只能操作excel
 
 ### POI包结构
 
 ```
 HSSF -- 读写XLS文件，老式文件 .xls
-XLSX -- 读写XLSX文件，新式文件 *** .xlsx文件 
+XSSF -- 读写XLSX文件，新式文件 *** .xlsx文件 
 HWPF -- 读写WORD DOC文件
 HSLF -- 读写PPT
+HDGF -- visio文件
+HPBF -- publisher文件
+HSMF -- outlook文件
 ```
 
 ### 同类技术对比
@@ -28,11 +33,7 @@ HSLF -- 读写PPT
 1. XSSFWorkbook: 工作簿
 2. XSSFSheet: 工作表
 3. Row: 行
-4. Cell: 单元格
-
-## 案例
-
-### 读
+4. Cell: 单元格读
 
 
 
--- src/main/java/cn/ylj/readDemo.java
+++ src/main/java/cn/ylj/readDemo.java
@@ -4,6 +4,8 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -18,19 +20,24 @@ public class readDemo {
 
     public static void main(String[] args) throws IOException {
         //获取maven目录resource下的资源目录
-        String path = readDemo.class.getClassLoader().getResource("read.xlsx").getPath();
+        String path = readDemo.class.getClassLoader().getResource("test.xlsx").getPath();
         //1.获取工作簿对象
         XSSFWorkbook wb = new XSSFWorkbook(path);
         //2.获取工作表
         //XSSFSheet sheet1 = wb.getSheet("sheet1");//根据工作表名称获取
         XSSFSheet sheet = wb.getSheetAt(0);//根据工作表索引获取
-        //3.获取行
-        for (Row row : sheet) {
-            //4.获取单元格
-            for (Cell cell : row) {
-                String value = cell.getStringCellValue();
-                System.out.println(value);
+
+        //3.获取行， (获取的是最后一行的索引从0开始)
+        int lastRowNum = sheet.getLastRowNum();
+        for (int i = 0; i <= lastRowNum; i++) {
+            XSSFRow row = sheet.getRow(i);
+            //4.获取列 (获取的列的数量从1开始)
+            short lastCellNum = row.getLastCellNum();
+            for (int j = 0; j < lastCellNum; j++) {
+                XSSFCell cell = row.getCell(j);
+                System.out.println(cell.getStringCellValue());
             }
+            System.out.println();
         }
         //5.释放资源
         wb.close();
